652c45e66b63250c8e1f0213e732119faf38edd4
Jarno Rantanen <jarno@jrw.fi>
2018-01-28 12:35:59 +0200

    Add proxy utils.

---
+++ utils/proxy.spec.ts
@@ -0,0 +1,122 @@
+import 'mocha';
+import { assert } from 'chai';
+import { proxyRequest, DEFAULT_TIMEOUT } from './proxy';
+
+describe('utils/proxy', () => {
+  describe('proxyRequest()', () => {
+    const SAMPLE_REQUEST = {
+      requestId: '62c362a2-f949-11e7-907f-bf2bff7069b0',
+      requestMethod: 'POST',
+      requestPath: '/test/request',
+      requestParams: { foo: 'bar' },
+      requestHeaders: {
+        'Accept-Encoding': 'gzip, deflate',
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'text/plain',
+        'User-Agent': 'Amazon CloudFront',
+      },
+      requestBody: 'Hello Lambda!',
+    };
+
+    const OUTGOING_URLS = ['http://one.com/', 'http://two.com/'];
+
+    const PROXIED_HEADERS = [
+      'authorization',
+      'content-type',
+      'user-agent',
+      'x-request-id',
+    ];
+
+    it('calls axios with correct params', () => {
+      const axiosArgs: object[] = [];
+      const fakeAxios = {
+        request(args: object) {
+          axiosArgs.push(args);
+          return Promise.resolve({ status: 200 });
+        },
+      } as any;
+      return proxyRequest(
+        SAMPLE_REQUEST,
+        OUTGOING_URLS,
+        PROXIED_HEADERS,
+        undefined,
+        fakeAxios,
+      ).then(() => {
+        assert.deepEqual(axiosArgs, [
+          {
+            url: 'http://one.com/',
+            method: 'POST',
+            data: 'Hello Lambda!',
+            headers: {
+              'Content-Type': 'text/plain',
+              'User-Agent': 'Amazon CloudFront',
+              'X-Request-ID': '62c362a2-f949-11e7-907f-bf2bff7069b0',
+            },
+            params: {
+              foo: 'bar',
+            },
+            timeout: DEFAULT_TIMEOUT,
+          },
+          {
+            url: 'http://two.com/',
+            method: 'POST',
+            data: 'Hello Lambda!',
+            headers: {
+              'Content-Type': 'text/plain',
+              'User-Agent': 'Amazon CloudFront',
+              'X-Request-ID': '62c362a2-f949-11e7-907f-bf2bff7069b0',
+            },
+            params: {
+              foo: 'bar',
+            },
+            timeout: DEFAULT_TIMEOUT,
+          },
+        ]);
+      });
+    });
+
+    it('resolves with the result of all operations', () => {
+      const fakeAxios = {
+        request(args: any) {
+          return args.url === 'http://one.com/'
+            ? Promise.resolve({
+                status: 200,
+                statusText: 'OK',
+                data: null,
+                headers: {},
+              })
+            : Promise.reject({
+                response: {
+                  status: 404,
+                  statusText: 'Not Found',
+                  data: null,
+                  headers: {},
+                },
+              });
+        },
+      } as any;
+      return proxyRequest(
+        SAMPLE_REQUEST,
+        OUTGOING_URLS,
+        [],
+        undefined,
+        fakeAxios,
+      ).then(res => {
+        assert.deepEqual(res, {
+          'http://one.com/': {
+            status: 200,
+            statusText: 'OK',
+            data: null,
+            headers: {},
+          },
+          'http://two.com/': {
+            status: 404,
+            statusText: 'Not Found',
+            data: null,
+            headers: {},
+          },
+        });
+      });
+    });
+  });
+});
---
+++ utils/proxy.ts
@@ -0,0 +1,81 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import { IncomingRequest, Headers } from './lambda';
+
+export const DEFAULT_TIMEOUT = 10000;
+
+export interface ProxyResponse {
+  data: any;
+  status: number;
+  statusText: string;
+  headers: Headers;
+}
+
+export function proxyRequest(
+  incomingRequest: IncomingRequest,
+  outgoingUrls: string[],
+  proxiedHeaders: string[] = [],
+  timeoutAfter: number = DEFAULT_TIMEOUT,
+  axiosOverride: AxiosInstance = axios,
+): Promise<{ [outgoingUrl: string]: ProxyResponse }> {
+  return Promise.all(
+    outgoingUrls.map(outgoingUrl =>
+      axiosOverride
+        .request({
+          url: outgoingUrl,
+          method: incomingRequest.requestMethod,
+          data: incomingRequest.requestBody,
+          params: incomingRequest.requestParams,
+          headers: filterHeaders(
+            {
+              'X-Request-ID': incomingRequest.requestId,
+              ...incomingRequest.requestHeaders,
+            },
+            proxiedHeaders,
+          ),
+          timeout: timeoutAfter,
+        })
+        .then(simplifyResponse)
+        .catch(err => {
+          // @see https://github.com/axios/axios#handling-errors
+          if (err.response) return simplifyResponse(err.response); // The request was made and the server responded with a status code that falls out of the range of 2xx
+          if (err.request) return errorResponse('Request Timed Out'); // The request was made but no response was received
+          throw new Error(`Could not proxy request: ${err}`); // Something happened in setting up the request that triggered an Error
+        })
+        .then(result => ({ [outgoingUrl]: result })),
+    ),
+  ).then(results =>
+    results.reduce((memo, next) => Object.assign(memo, next), {}),
+  );
+}
+
+export function filterHeaders(
+  headers: Headers,
+  proxiedHeaders: string[],
+): Headers {
+  return Object.keys(headers)
+    .map(key => [key, headers[key]])
+    .reduce(
+      (memo, next) => {
+        const [key, val] = next;
+        if (proxiedHeaders.indexOf(key.toLowerCase()) !== -1) {
+          memo[key] = val;
+        }
+        return memo;
+      },
+      {} as Headers,
+    );
+}
+
+function simplifyResponse(response: AxiosResponse): ProxyResponse {
+  const { data, status, statusText, headers } = response;
+  return { data, status, statusText, headers };
+}
+
+function errorResponse(statusText: string): ProxyResponse {
+  return {
+    data: null,
+    status: 0,
+    statusText,
+    headers: {},
+  };
+}
