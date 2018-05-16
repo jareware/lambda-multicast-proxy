627c49fb58a18484f86ebba180e40fca6b55c05e
Jarno Rantanen <jarno@jrw.fi>
2018-05-17 00:44:05 +0300

    Add first iteration of createPapertrailLogger().

--- utils/logging.ts
+++ utils/logging.ts
@@ -1,5 +1,7 @@
 import { IncomingRequest, LambdaResponse } from './lambda';
 import { ProxyResponseMap } from './proxy';
+import { connect, TLSSocket } from 'tls';
+import { inspect } from 'util';
 
 export type LogLevel = 'debug' | 'info' | 'warn' | 'error';
 export type LoggerMethod = (message: string, meta?: any) => void;
@@ -56,5 +58,133 @@ export function logProxiedRequest(
   });
   if (outgoing) {
     log.debug('Outgoing response:', { outgoing });
+  }
+}
+
+export function createPapertrailLogger(
+  papertrailHost: string,
+  papertrailPort: number,
+  hostName: string,
+  appName: string,
+  logLevel: LogLevel | null = 'debug',
+): Logger {
+  const socket = createPapertrailSocket(papertrailHost, papertrailPort);
+  const bind = (currentLevel: LogLevel): LoggerMethod => {
+    if (level(currentLevel) < level(logLevel)) return () => {};
+    return (message, meta) => {
+      socket.send(
+        (message + (typeof meta === 'undefined' ? '' : '\n' + formatMeta(meta)))
+          .split('\n')
+          .map(line =>
+            formatPapertrailLine(currentLevel, hostName, appName, line),
+          )
+          .join(''),
+      );
+    };
+  };
+  return {
+    debug: bind('debug'),
+    info: bind('info'),
+    warn: bind('warn'),
+    error: bind('error'),
+  };
+}
+
+function formatMeta(meta: any) {
+  return inspect(meta, {
+    showHidden: false,
+    depth: 5,
+    colors: true,
+    customInspect: true,
+    showProxy: false,
+    maxArrayLength: 100,
+    breakLength: 60,
+    compact: true,
+  } as any); // @types/node doesn't know of "compact"
+}
+
+// http://tools.ietf.org/html/rfc5424#section-6
+const PAPERTRAIL_PRIVALS: { [level in LogLevel]: number } = {
+  debug: 31,
+  info: 30,
+  warn: 28,
+  error: 27,
+};
+
+function formatPapertrailLine(
+  level: LogLevel,
+  hostName: string,
+  appName: string,
+  line: string,
+) {
+  const prival = PAPERTRAIL_PRIVALS[level];
+  const ts = new Date().toISOString();
+  return `<${prival}>1 ${ts} ${hostName} ${appName} - - - [${level}] ${line}\n`;
+}
+
+// Creates a TLS socket connection to Papertrail.
+function createPapertrailSocket(
+  papertrailHost: string,
+  papertrailPort: number,
+  log = createConsoleLogger('warn'),
+) {
+  const start = connectToPapertrail();
+
+  start
+    .then(socket => {
+      log.info('Connected to Papertrail');
+      socket.setTimeout(60 * 1000);
+      socket.on('timeout', () => socket.write(''));
+    })
+    .catch(err => log.error('Could not connect to Papertrail', err));
+
+  return {
+    // Sends given syslog-formatted data to Papertrail.
+    send,
+
+    // Closes the socket, and returns a Promise that resolves when all data has been sent,
+    // or rejects if some data may have been lost due to e.g. connection loss.
+    close,
+  };
+
+  function connectToPapertrail() {
+    return new Promise<TLSSocket>((resolve, reject) => {
+      const socket = connect({ host: papertrailHost, port: papertrailPort }); // https://nodejs.org/docs/latest-v8.x/api/tls.html#tls_tls_connect_options_callback
+      socket.setKeepAlive(true);
+      socket.once('secureConnect', () => resolve(socket));
+      socket.once('error', reject);
+    });
+  }
+
+  function send(data: string): void {
+    log.debug('Sending data to Papertrail');
+    start
+      .then(socket =>
+        socket.write(data + '\n', () => log.debug('Sent data to Papertrail')),
+      )
+      .catch(error =>
+        log.error('Could not send data to Papertrail', { data, error }),
+      );
+  }
+
+  function close(): Promise<null> {
+    return start.then(socket => {
+      log.debug('Papertrail socket is closing');
+      socket.end();
+      return new Promise<null>((resolve, reject) => {
+        socket.once('close', hadTransmissionError => {
+          if (hadTransmissionError) {
+            const err = new Error(
+              'Papertrail socket has closed, but not all data may have been sent',
+            );
+            log.warn(err.message);
+            reject(err);
+          } else {
+            log.info('Papertrail socket has closed cleanly');
+            resolve(null);
+          }
+        });
+      });
+    });
   }
 }
